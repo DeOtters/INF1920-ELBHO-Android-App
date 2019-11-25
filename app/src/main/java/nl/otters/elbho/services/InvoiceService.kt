49bb02150c513c2dee0d6e6d752c486ab97560fe
Daniël Kroese <daniel@kroese.com>
2019-11-25 14:07:49 +0100

    Added Invoice service and repository

--- app/src/main/java/nl/otters/elbho/services/InvoiceService.kt
+++ app/src/main/java/nl/otters/elbho/services/InvoiceService.kt
@@ -1,6 +1,5 @@
 package nl.otters.elbho.services
 
-import nl.otters.elbho.models.Adviser
 import nl.otters.elbho.models.Invoice
 import retrofit2.Call
 import retrofit2.http.*
@@ -9,17 +8,17 @@ interface InvoiceService {
 
     @Headers("Content-type: application/json")
     @GET("advisor/me/invoices")
-    fun getAllInvoices(@Header("x-jwt") auth: Adviser.Authentication): Call<ArrayList<Invoice>>
+    fun getAllInvoices(@Header("x-jwt") auth: String): Call<ArrayList<Invoice>>
 
     @Headers("Content-type: application/json")
     @GET("/invoices/{id}")
-    fun getInvoice(@Header("x-jwt") auth: Adviser.Authentication, @Path("id") id: Int): Call<Invoice>
+    fun getInvoice(@Header("x-jwt") auth: String, @Path("id") id: Int): Call<Invoice>
 
     @Headers("Content-type: application/json")
     @POST("/invoices")
-    fun createInvoice(@Header("x-jwt") auth: Adviser.Authentication, @Body invoice: Invoice): Call<Unit>
+    fun createInvoice(@Header("x-jwt") auth: String, @Body invoice: Invoice): Call<Unit>
 
     @Headers("Content-type: application/json")
     @PUT("/invoices/{id}")
-    fun updateInvoice(@Header("x-jwt") auth: Adviser.Authentication, @Path("id") id: Int, @Body invoice: Invoice): Call<Unit>
+    fun updateInvoice(@Header("x-jwt") auth: String, @Path("id") id: Int, @Body invoice: Invoice): Call<Unit>
 }
