acf23976b4755f950bfc5c13441d75be3f11f66a
juanalb <juan@zimmermanzimmerman.com>
2019-11-30 22:24:08 +0100

    feat: implemented UI Login Activity
    TODO:
    -underline color
    -password input text hidden
    -button disabled state
    
    REFACTOR:
    -create custom components directory and add textfield

---
+++ app/src/main/java/nl/otters/elbho/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package nl.otters.elbho.activities
+
+import nl.otters.elbho.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle? ) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
