b28d8ac9a359c46ab0edb39aa5b08580c9d8b62e
danielkroese <daniel@kroese.com>
2019-12-04 03:01:54 +0100

    Fixed it!!d

--- app/src/main/java/nl/otters/elbho/activities/NavigationActivity.kt
+++ app/src/main/java/nl/otters/elbho/activities/NavigationActivity.kt
@@ -9,7 +9,8 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.navigation.NavigationView
 import nl.otters.elbho.R
 
-abstract class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+abstract class NavigationActivity : AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
@@ -21,33 +22,38 @@ abstract class NavigationActivity : AppCompatActivity(), NavigationView.OnNaviga
         return when (item.itemId) {
             R.id.open_requests -> {
                 val intent = Intent(this, OverviewActivity::class.java)
-                Toast.makeText(this, "Overview", Toast.LENGTH_SHORT).show()
                 this.startActivity(intent)
+                Toast.makeText(this, "Overview", Toast.LENGTH_SHORT).show()
                 true
             }
             R.id.upcoming_requests -> {
                 val intent = Intent(this, OverviewActivity::class.java)
                 this.startActivity(intent)
+                Toast.makeText(this, "Overview", Toast.LENGTH_SHORT).show()
                 true
             }
             R.id.done_requests -> {
                 val intent = Intent(this, OverviewActivity::class.java)
                 this.startActivity(intent)
+                Toast.makeText(this, "Overview", Toast.LENGTH_SHORT).show()
                 true
             }
             R.id.availability -> {
                 val intent = Intent(this, AvailabilityActivity::class.java)
                 this.startActivity(intent)
+                Toast.makeText(this, "Availability", Toast.LENGTH_SHORT).show()
                 true
             }
             R.id.vehicle -> {
                 val intent = Intent(this, VehicleActivity::class.java)
                 this.startActivity(intent)
+                Toast.makeText(this, "Vehicle", Toast.LENGTH_SHORT).show()
                 true
             }
             R.id.invoice -> {
-                val intent = Intent(this, LoginActivity::class.java)
+                val intent = Intent(this, InvoiceActivity::class.java)
                 this.startActivity(intent)
+                Toast.makeText(this, "Invoice", Toast.LENGTH_SHORT).show()
                 true
             }
             else -> super.onOptionsItemSelected(item)
--- app/src/main/java/nl/otters/elbho/activities/RequestActivity.kt
+++ app/src/main/java/nl/otters/elbho/activities/RequestActivity.kt
@@ -1,11 +1,13 @@
 package nl.otters.elbho.activities
 
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_overview.*
 import nl.otters.elbho.R
 
 class RequestActivity : NavigationActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_request)
+        navigation.setNavigationItemSelectedListener(this)
     }
 }
