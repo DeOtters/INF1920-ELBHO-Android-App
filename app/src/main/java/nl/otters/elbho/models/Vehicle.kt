294b683870b3fb819af62036681de6a6ff73cd34
Alex Abdalla <alex.abdalla@divault.nl>
2019-12-08 13:11:17 +0100

    Vehicle start

--- app/src/main/java/nl/otters/elbho/models/Vehicle.kt
+++ app/src/main/java/nl/otters/elbho/models/Vehicle.kt
@@ -10,8 +10,8 @@ object Vehicle {
         @SerializedName("licenseplate")
         val licensePlate: String,
         val brand: String,
-        val type: String,
-        val transmission: String,
+        val Model: String,
+        val transmission: Boolean,
         val color: String,
         val location: String,
         val picture: String
@@ -20,11 +20,11 @@ object Vehicle {
     data class Reservation(
 
         @SerializedName("vehicle-id")
-        val vehicleId: Int,
+        val vehicleId: String,
         @SerializedName("user-id")
-        val userId: Int,
+        val userId: String,
         @SerializedName("request-id")
-        val requestId: Int,
+        val requestId: String,
         @SerializedName("datetimeslot")
         val dateTimeSlot: DateTimeSlot
     )
