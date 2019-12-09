c6ff459c963a88951671e35108719a9997ff9439
juanalb <juan@zimmermanzimmerman.com>
2019-12-09 21:38:26 +0100

    feat: implemented data retrieval availability

--- app/src/main/java/nl/otters/elbho/views/fragments/AvailabilityFragment.kt
+++ app/src/main/java/nl/otters/elbho/views/fragments/AvailabilityFragment.kt
@@ -1,14 +1,30 @@
 package nl.otters.elbho.views.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_availability.*
 import nl.otters.elbho.R
+import nl.otters.elbho.models.Availability
+import nl.otters.elbho.repositories.AvailabilityRepository
+import nl.otters.elbho.utils.DateParser
+import nl.otters.elbho.viewModels.AvailabilityViewModel
 
 class AvailabilityFragment : Fragment() {
+    private var availability: ArrayList<Availability.Slot> = ArrayList()
+    private val dateParser: DateParser = DateParser()
+    //Eerst moet ik een lijst met de availability ophalen van de DB CHECK
+    //TODO: Vervolgens moet ik deze data vullen in de calendar
+    //TODO: Ook moet er een OnDateChangeListener toegevoegd
+        // Pak de datum
+        // Pak de week van deze datum
+        // Stuur die mee als bundle naar de volgende pagina
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +32,23 @@ class AvailabilityFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_availability, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        val availabilityRepository = AvailabilityRepository(activity!!.applicationContext)
+        val availabilityViewModel = AvailabilityViewModel(availabilityRepository)
+
+        availabilityViewModel.getAllAvailabilities()?.observe(this, Observer {
+            availability = it
+            Log.e("it", it.toString())
+        })
+
+
+        super.onActivityCreated(savedInstanceState)
+        calendarView.setOnDateChangeListener { calenderView: CalendarView, year: Int, month: Int, day: Int ->
+            // availability van die week doorsturen
+            // week doorsturen
+        }
     }
 
     override fun onResume() {
