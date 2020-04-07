71f0ae44e142162c43d7defa0441a268d49f39e4
danielkroese <daniel@kroese.com>
2020-04-07 19:36:19 +0200

    Correct nav highlighted on tablet when navigating back from detail view with system back button

--- app/src/main/java/nl/otters/elbho/views/fragments/OverviewFragment.kt
+++ app/src/main/java/nl/otters/elbho/views/fragments/OverviewFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_overview.*
 import nl.otters.elbho.R
 import nl.otters.elbho.adapters.ViewPagerAdapter
@@ -18,7 +20,7 @@ import nl.otters.elbho.viewModels.OverviewViewModel
 import nl.otters.elbho.views.activities.NavigationActivity
 
 
-class OverviewFragment : BaseFragment() {
+class OverviewFragment : BaseFragment(), TabLayout.OnTabSelectedListener {
     private lateinit var requestRepository: RequestRepository
     private lateinit var appointmentRepository: AppointmentRepository
     private lateinit var overviewViewModel: OverviewViewModel
@@ -26,6 +28,7 @@ class OverviewFragment : BaseFragment() {
     private var requests: ArrayList<Request.Properties> = ArrayList()
     private var todaysAppointments: ArrayList<Request.Properties> = ArrayList()
     private val dateParser: DateParser = DateParser()
+    private var tabIndex: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -100,7 +103,34 @@ class OverviewFragment : BaseFragment() {
         tabs.getTabAt(0)!!.setIcon(R.drawable.ic_event_available_24dp)
         tabs.getTabAt(1)!!.setIcon(R.drawable.ic_event_24dp)
         tabs.getTabAt(2)!!.setIcon(R.drawable.ic_event_done_24dp)
-        tabs.getTabAt(arguments!!.get("tabId") as Int)!!.select()
+        val tabIndex = arguments!!.get("tabId") as Int
+        tabs.getTabAt(tabIndex)!!.select()
+        selectNavigationItem()
         viewPager.setPageTransformer(true, ZoomOutPageTransformer())
+    }
+
+    private fun selectNavigationItem() {
+        val navigation = activity!!.findViewById<View>(R.id.navigation) as NavigationView
+        when (tabIndex) {
+            0 -> {
+                navigation.setCheckedItem(R.id.open_requests)
+            }
+            1 -> {
+                navigation.setCheckedItem(R.id.upcoming_requests)
+            }
+            2 -> {
+                navigation.setCheckedItem(R.id.done_requests)
+            }
+        }
+    }
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab?) {
+    }
+
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        tabIndex = tab?.position ?: 0
     }
 }
